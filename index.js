c4ea1bab207687318738251719ebf469441ec3ac
thuotracy <tracyjacobs775@gmail.com>
2023-08-22 12:17:44 +0300

    made changes

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ for ( var i = 0; i<emojis.length; i++){
             if(document.querySelectorAll('.boxOpen').length > 1){
                 if(document.querySelectorAll('.boxOpen')[0].innerHTML ==
                 document.querySelectorAll('.boxOpen')[1].innerHTML){
-                    document.querySelectorAll('.boxOpen')[0].classList.add('.boxMatch')
-                    document.querySelectorAll('.boxOpen')[1].classList.add('.boxMatch')
-                    document.querySelectorAll('.boxOpen')[1].classList.remove('.boxOpen')
-                    document.querySelectorAll('.boxOpen')[0].classList.remove('.boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
 
                     if(document.querySelectorAll('.boxMatch').length == emojis.length){
                         alert('WIN')
                     }
                 } else{
-                    document.querySelectorAll('.boxOpen')[1].classList.remove('.boxOpen')
-                    document.querySelectorAll('.boxOpen')[0].classList.remove('.boxOpen')
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
                 }
             }
         },500)
