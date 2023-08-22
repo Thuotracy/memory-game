2c05edd9f3f350d1de692be987b8cf657534b132
thuotracy <tracyjacobs775@gmail.com>
2023-08-22 11:47:13 +0300

    made changes

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ for ( var i = 0; i<emojis.length; i++){
     let box = document.createElement("div")
     box.className = 'item';
     box.innerHTML = shuf_emojis[i]
+    document.querySelector('.game').appendChild(box);
 }
 
