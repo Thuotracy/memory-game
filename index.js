52e58dce7197cabb9bf3adfa1a3e2fa7d0a15823
thuotracy <tracyjacobs775@gmail.com>
2023-08-22 12:22:17 +0300

    made changes

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ for ( var i = 0; i<emojis.length; i++){
         this.classList.add('boxOpen')
         setTimeout(function(){
             if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll('.boxOpen')[0].innerHTML ==
-                document.querySelectorAll('.boxOpen')[1].innerHTML){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
                     document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
                     document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
