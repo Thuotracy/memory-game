06a18d0996f45a755a893d220d29f93a3bb2d843
thuotracy <tracyjacobs775@gmail.com>
2023-08-22 11:43:43 +0300

    made changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const emojis = ["😃","😃","🥶","🥶","😚","😚","❤️‍🔥","❤️‍🔥","😶","😶","🍩","🍩","😶‍🌫️","😶‍🌫️","🤫","🤫"];
 var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+for ( var i = 0; i<emojis.length; i++){
+    let box = document.createElement("div")
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+}
 
